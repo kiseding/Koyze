d0b48696e216cde45093299b0b80624124c85587
yys <yys@koyze.local>
2026-08-22 00:22:38 +0800

    fix: restore desktop directory picker import

--- lib/features/local_music/presentation/local_music_screen.dart
+++ lib/features/local_music/presentation/local_music_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:permission_handler/permission_handler.dart';
