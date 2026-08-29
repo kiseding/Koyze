796bc1c04e4a70a0a68895ad85544be3b0647d96
yys <yys@koyze.local>
2026-08-29 13:25:57 +0800

    ci: validate renamed Android artifacts per file

--- test/build_configuration_test.dart
+++ test/build_configuration_test.dart
@@ -179,10 +179,10 @@ void main() {
     expect(signingCheck, lessThan(analyze));
     expect(workflow, contains('--split-per-abi'));
     expect(workflow, contains('Koyze-\${version}-\${abi}.apk'));
-    expect(workflow, contains('build/android-release/Koyze-*.apk'));
+    expect(workflow, contains('for apk in build/android-release/*.apk'));
     expect(workflow, contains('Koyze-Android-APKs'));
     expect(workflow, contains('flutter build appbundle --release'));
-    expect(workflow, contains('build/android-release/Koyze-*.aab'));
+    expect(workflow, contains('for aab in build/android-release/*.aab'));
     expect(workflow, contains('Koyze-Android-AAB'));
     expect(workflow, contains('ANDROID_KEYSTORE_BASE64'));
     expect(workflow, isNot(contains('ANDROID_KEYSTORE_BASE64 !=')));
